3ea85d967376c65627fecdcddbbc2b4f79071427
BasikBiceps <nokem.noks@gmail.com>
2019-09-18 19:53:19 +0300

    Added tea boolean, fixed home page

---
+++ TicketsDemo.Domain/DefaultImplementations/TeaPriceCalculationStrategy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketsDemo.Data.Entities;
+using TicketsDemo.Data.Repositories;
+using TicketsDemo.Domain.Interfaces;
+
+namespace TicketsDemo.Domain.DefaultImplementations.PriceCalculationStrategy
+{
+    public class TeaPriceCalculationStrategy : IPriceCalculationStrategy
+    {
+        private IPriceCalculationStrategy _defaultPriceCalculationStrategy;
+
+        public TeaPriceCalculationStrategy(IPriceCalculationStrategy defaultPriceCalculationStrategy)
+        {
+            _defaultPriceCalculationStrategy = defaultPriceCalculationStrategy;
+        }
+
+        public List<PriceComponent> CalculatePrice(PlaceInRun placeInRun)
+        {
+            var priceComponents = _defaultPriceCalculationStrategy.CalculatePrice(placeInRun);
+
+            var teaComponent = new PriceComponent()
+            {
+                Name = "Price for tea",
+                Value = 50
+            };
+            priceComponents.Add(teaComponent);
+
+            return priceComponents;
+        }
+    
+    }
+}
